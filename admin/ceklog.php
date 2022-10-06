e3c3014ecd10505283ee00aa5aca7313e3e3ac1c
unknown <adeliyaalvirahmanda@gmail.com>
2022-10-06 10:02:31 +0700

    fix login

---
+++ admin/ceklog.php
@@ -0,0 +1,24 @@
+<?php
+	session_start();
+	include 'koneksi.php';
+
+	$username = $_POST['username'];
+    $password = $_POST['pass'];
+    
+    $data=mysqli_query($koneksi,"select * from admin where username='$username' and pass='$password'");
+
+    $cek = mysqli_num_rows($data);
+
+    if($cek > 0){
+        $_SESSION['username'] = $username;
+        $_SESSION['status'] = "login";
+        header("location:welcome.php");
+    }else{
+        var_dump($username);
+        
+        var_dump($password);
+        var_dump($_SESSION);
+    }
+    session_destroy();
+
+?>
---
+++ admin/koneksi.php
@@ -0,0 +1,17 @@
+<?php 
+
+$host="localhost";
+$user="root";
+$password="";
+$database="perpuskita";
+
+$koneksi = mysqli_connect($host, $user, $password, $database);
+
+//cek koneksi
+if ($koneksi) {
+	echo "Berhasil Terhubung";
+} else {
+	echo "Gagal Terhubung";
+}
+ 
+?>
